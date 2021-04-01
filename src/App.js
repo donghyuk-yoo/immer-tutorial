b55f42652630b15a8aa427dc82593796ced7b11c
I an <ydh8485@gmail.com>
2021-04-02 06:13:55 +0900

    immer with functional update(useState)

--- src/App.js
+++ src/App.js
@@ -9,22 +9,19 @@ const App = () => {
     uselessValue: null,
   });
 
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setFrom(
-        // ...연산자를 이용한 업데이트
-        // {...form,
-        // [name]: [value],}
-        // immer 사용 업데이트, 불변성에 신경쓰지 않고 작성하되,
-        // 실은 immer가 불변성을 유지하며 새로운 상태를 생성해 줌
-        produce(form, (draft) => {
-          draft[name] = value;
-        })
-      );
-    },
-    [form]
-  );
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFrom(
+      // ...연산자를 이용한 업데이트
+      // {...form,
+      // [name]: [value],}
+      // immer 사용 업데이트, 불변성에 신경쓰지 않고 작성하되,
+      // 실은 immer가 불변성을 유지하며 새로운 상태를 생성해 줌
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
 
   const onSubmit = useCallback(
     (e) => {
@@ -38,7 +35,7 @@ const App = () => {
       setData(
         // {...data,
         // array: data.array.concat(info),}
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         })
       );
@@ -49,27 +46,24 @@ const App = () => {
       });
       nextId.current = nextId.current + 1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        // {
-        //   ...data,
-        //   array: data.array.filter((info) => info.id !== id),
-        // }
-        // 오히려 immer사용시 더 복잡해지는 경우도 있다. 이럴경우 굳이 사용할 필요x
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1
-          );
-        })
-      );
-    },
-    [data]
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      // {
+      //   ...data,
+      //   array: data.array.filter((info) => info.id !== id),
+      // }
+      // 오히려 immer사용시 더 복잡해지는 경우도 있다. 이럴경우 굳이 사용할 필요x
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <div>
